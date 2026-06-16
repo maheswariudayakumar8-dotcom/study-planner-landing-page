19976c40452724717ddf87e3ccbb4ea08bb342c9
BHAVANI U <maheswariudayakumar8@gmail.com>
2026-06-16 12:19:01 +0530

    add dark mode feature

--- script.js
+++ script.js
@@ -13,3 +13,18 @@ document.getElementById("contactForm").addEventListener("submit", function(e) {
         msg.style.color = "green";
     }
 });
+const toggleBtn = document.getElementById("toggleDark");
+
+toggleBtn.addEventListener("click", function () {
+    document.body.classList.toggle("dark");
+
+    if (document.body.classList.contains("dark")) {
+        localStorage.setItem("theme", "dark");
+    } else {
+        localStorage.setItem("theme", "light");
+    }
+});
+
+if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark");
+}
